50fba2fb702d50d9583df6459df81b3b24c07026
Aishwarya Khandare <aishwaryakhandare005@gmail.com>
2026-06-02 22:27:50 +0530

    Insights file created

---
+++ pages/Insights.jsx
@@ -0,0 +1,14 @@
+function Insights() {
+
+    return (
+
+        <div>
+        
+        <h1>Insights</h1>
+
+        <p>Should youu go out?</p>
+
+        </div>
+    )
+}
+export default Insights
