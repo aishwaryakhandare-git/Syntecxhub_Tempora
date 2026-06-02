f6c8aa6d87976bb6a355cb307e23a55d3b1c6ae8
Aishwarya Khandare <aishwaryakhandare005@gmail.com>
2026-06-02 22:48:02 +0530

    Overview page css

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,17 +1,62 @@
-function Overview() {
+import "../styles/overview.css"
 
-return (
+function Overview(){
 
-<div>
+return(
 
-<h1>Tempora</h1>
+<div className="overview">
 
-<h2>Good Morning ☀</h2>
+<div className="hero">
+
+<p className="logo">
+TEMPORA
+</p>
+
+<input
+
+className="search"
+
+placeholder="Search a city"
+
+/>
+
+</div>
+
+
+<div className="weather">
+
+<h1>
+28°
+</h1>
 
 <p>
-See the sky.
-Plan your day.
+Partly Cloudy
 </p>
+
+<p>
+Mumbai
+</p>
+
+</div>
+
+
+<div className="story">
+
+<h2>
+Today's Weather Story
+</h2>
+
+<p>
+
+Warm morning.
+
+Humidity may rise later.
+
+Good weather for short walks.
+
+</p>
+
+</div>
 
 </div>
 
